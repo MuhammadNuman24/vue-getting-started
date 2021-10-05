6d819b91422972cc403e3d79d4d4015477249f63
Muhammad Numan <numanrazzaq24@gmail.com>
2021-10-05 11:35:52 +0500

    Calling Api

--- 07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ 07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -1,14 +1,30 @@
 import * as axios from 'axios';
 import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
+import { API } from './config';
 
 const getHeros = async function() {
-  const responce = await axios.get('api/heroes.json');
-  const heroes = responce.data.map(h => {
-    h.originDate = format(h.origin, inputDateFormat);
-    return h;
-  });
-  return heroes;
+  try {
+    const response = axios.get(`${API}/heroes.json`);
+    let data = parseList(response);
+    const heroes = data.map(h => {
+      h.originDate = format(h.originDate, inputDateFormat);
+      return h;
+    });
+    return heroes;
+  } catch (error) {
+    console.error('this is error message');
+    return [];
+  }
+};
+const parseList = response => {
+  if (response.status != 200) throw Error(response.message);
+  if (!response.data) return [];
+  let list = response.data;
+  if (typeof list != 'object') {
+    list = [];
+  }
+  return list;
 };
 
 export const data = {
